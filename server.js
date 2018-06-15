5673ab05a33e7a2cb9fc5967f4374f0549e8247f
Radhika <radhikaiyer@Radhikas-Air.attlocal.net>
2018-06-15 11:19:35 -0700

    styling and layout

---
+++ server.js
@@ -0,0 +1,46 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cheerio = require("cheerio");
+var request = require("request");
+var axios = require("axios");
+
+// Set up port
+var PORT = process.env.PORT || 3000;
+
+// Set up Express App
+var app = express();
+
+// Require routes
+var routes = require("./routes");
+
+// Designate our public folder as a static directory
+app.use(express.static("public"));
+
+// Connect Handlebars to Express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Use bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Have every request go through route middleware
+app.use(routes);
+
+// Use the deployed database or local
+var MONGODB_URI = process.env.MONGODB_URI ||
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
+// Listen on the port
+app.listen(PORT, function() {
+  console.log("Listening on port: " + PORT);
+});
